a0b8b24f4d44397739de7ef63d90c1abbfe3d725
yurimarine <brandocalvento@gmail.com>
2026-03-15 15:33:25 +0800

    Second Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-800">Hello Tailwind CSS!</h1>
+      <h1 className="text-4xl font-bold text-blue-800">Another Change</h1>
     </main>
   );
 }
